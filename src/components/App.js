5f95497c87fc8a18aaab0edbecaf684244cb25ce
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-01 11:06:31 -0400

    install redux, import connect, wire connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 // titling file in ./actions as index.js means we can use cleaner syntax below
 // import { selectSong } from '../actions'   (not necessary in this case)
+import SongList from './SongList'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <SongList />
+    </div>
+  )
 }
 
 export default App
