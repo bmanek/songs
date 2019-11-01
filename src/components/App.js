9fbd994c9ff8aabfdf6c0c1a37d145f28eefaa47
bmanek <41875460+bmanek@users.noreply.github.com>
2019-11-01 10:22:41 -0400

    add actions, reducers folder, write selectSong action creator

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// titling file in ./actions as index.js means we can use cleaner syntax below
+// import { selectSong } from '../actions'   (not necessary in this case)
 
 const App = () => {
   return <div>App</div>
